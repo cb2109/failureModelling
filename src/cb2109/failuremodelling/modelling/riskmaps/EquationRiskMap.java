73429d9fc496daa18d58d8851a9ba89fcf180725
Chris Bates <blokeyking@gmail.com>
2018-04-05 17:42:35 -0400

    Added Line intersection detection for circles

--- src/cb2109/failuremodelling/modelling/riskmaps/EquationRiskMap.java
+++ src/cb2109/failuremodelling/modelling/riskmaps/EquationRiskMap.java
@@ -131,7 +131,55 @@ public class EquationRiskMap implements RiskMap {
 
         @Override
         public boolean contains(Line l) {
-            return false;
+            // The following equation calculated intersection of a circle radius r and center (0, 0)
+            // because the center is 0, 0 we have to adjust the points
+            int x1 = l.p1.x - center.x;
+            int y1 = l.p1.y - center.y;
+            int x2 = l.p2.x - center.x;
+            int y2 = l.p2.y - center.y;
+            double dx = x2 - x1;
+            double dy = y2 - y1;
+            // this should be square rooted, but we always square it
+            double dr2 = dx * dx + dy * dy;
+            if (dr2 == 0) {
+                // 0 size line, so just check to see if p1 is inside
+                return contains(l.p1);
+            }
+            double determinant = x1 * y2 - x2 * y1;
+
+            double discriminant = radius * radius * dr2 - determinant * determinant;
+            // if the discriminant is < 0 then there is no intersection
+            if (discriminant < 0) {
+                return false;
+            }
+            double discriminantSqrt = Math.sqrt(discriminant);
+            double determinantDy = determinant * dy;
+            double determinantDx = determinant * dx;
+            int ySign = dy >= 0 ? 1 : -1;
+            // point 1
+            double xInt1 = (determinantDy + ySign * dx * discriminantSqrt) / dr2;
+            double yInt1 = (-1 * determinantDx + Math.abs(dy) * discriminantSqrt) / dr2;
+
+            // point 2
+            double xInt2 = (determinantDy - ySign * dx * discriminantSqrt) / dr2;
+            double yInt2 = (-1 * determinantDx - Math.abs(dy) * discriminantSqrt) / dr2;
+
+            // readjust the coordinates back to where the circle is actually positioned
+            double realXInt1 = xInt1 + center.x;
+            double realYInt1 = yInt1 + center.y;
+            double realXInt2 = xInt2 + center.x;
+            double realYInt2 = yInt2 + center.y;
+
+            int maxLineX = Math.max(l.p1.x, l.p2.x);
+            int minLineX = Math.min(l.p1.x, l.p2.x);
+            int maxLineY = Math.max(l.p1.y, l.p2.y);
+            int minLineY = Math.min(l.p1.y, l.p2.y);
+            // if point 1 or point 2 are contained then it should work
+            return minLineX <= realXInt1 && realXInt1 <= maxLineX
+                    && minLineY <= realYInt1 && realYInt1 <= maxLineY
+                    ||
+                    minLineX <= realXInt2 && realXInt2 <= maxLineX
+                    && minLineY <= realYInt2 && realYInt2 <= maxLineY;
         }
     }
 
